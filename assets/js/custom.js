681147756806617386428a309cf9a1d8449308d9
SahilGite4fox <sahilgite.4fox@gmail.com>
2025-10-27 13:11:12 +0530

    Single product page cards animation and page responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -39,19 +39,19 @@ $(document).ready(function () {
 
   // Accordion click
   $(".psAccHeading").click(function () {
-      var $content = $(this).next(".psAccContents");
+    var $content = $(this).next(".psAccContents");
 
-      // Toggle clicked content
-      $content.slideToggle(500);
+    // Toggle clicked content
+    $content.slideToggle(500);
 
-      // Close other contents
-      $(".psAccContents").not($content).slideUp(500);
+    // Close other contents
+    $(".psAccContents").not($content).slideUp(500);
 
-      // Toggle active class for clicked heading
-      $(this).toggleClass("psAccActive");
+    // Toggle active class for clicked heading
+    $(this).toggleClass("psAccActive");
 
-      // Remove active from other headings
-      $(".psAccHeading").not(this).removeClass("psAccActive");
+    // Remove active from other headings
+    $(".psAccHeading").not(this).removeClass("psAccActive");
   });
 
 
@@ -116,7 +116,7 @@ $(document).ready(function () {
 
 
   // solution tabing
-  $('.psNavTabs li a').click(function(e) {
+  $('.psNavTabs li a').click(function (e) {
     e.preventDefault();
 
     // Update tab active class
@@ -130,3 +130,80 @@ $(document).ready(function () {
   });
 
 });
+
+
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+const cards = gsap.utils.toArray(".custom-card");
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".cards",
+    pin: true,
+    pinSpacing: true,
+    markers: false,
+    start: "top-=200px top", // when the top of the trigger hits the top of the viewport
+    end: "+=2000", // end after scrolling 1000px beyond the start
+    scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+  }
+});
+
+cards.forEach((card, i) => {
+  const nextCard = cards[i + 1];
+
+  if (nextCard) {
+    // Animate the front card going backward (fade & scale down)
+    tl.to(card, {
+      yPercent: -30,
+      scale: 0.95,
+      opacity: 0.6,
+      zIndex: 8 - i,
+      ease: "power2.inOut"
+    });
+
+    // Animate the next card coming to front
+    tl.to(nextCard, {
+      yPercent: 0,
+      scale: 1,
+      opacity: 1,
+      zIndex: 9 - i,
+      ease: "power2.inOut"
+    }, "<"); // "<" keeps both animations in sync
+
+    tl.to(card, {
+      yPercent: -15,
+      scale: 0.95,
+      opacity: 1,
+      ease: "power2.inOut"
+    });
+  }
+});
+
+// Animation for card 2
+// tl.from('.card2', {  yPercent:0,  opacity: 1,}) 
+tl.addLabel("card2");
+// tl.add(() => setActiveNav(tl.scrollTrigger.direction > 0 ? 1 : 0), "-=0.15");
+// tl.to('.card1',{  scale:0.925,  yPercent:-0.75,  opacity: 1}, "-=0.3")
+// tl.to('.card2', {  yPercent:0,  opacity: 1})
+
+
+
+// Additional animations for scaling down previous cards
+// tl.to('.card1',{
+//   // scale:0.925,
+//   yPercent:-1.5,
+//   opacity: 0.9
+// }, "-=0.3")
+
+// tl.to('.card2',{
+//   // scale:0.95,
+//   yPercent:-1.125,
+//   opacity: 0.9
+// }, "-=0.3")
+
+
+// Without the .nav .circle elements, we don't need to handle setActiveNav function
+/* gsap.utils.toArray(".nav .circle").forEach((circle, i) => {
+  circle.classList[i === index ? "add" : "remove"]("active");
+}); */
